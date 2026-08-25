970a2fab1e913cd13d639f500e936fab146f162a
Umabharathi B <umabharathibalamurugan@gmail.com>
2026-08-25 11:06:20 +0530

    Fix STATIC_ROOT for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -133,6 +133,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / 'frontend']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
